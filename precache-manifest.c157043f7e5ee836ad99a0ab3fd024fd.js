477956d5aea110cedb3e7901f5ead061ea2dd6e0
David Bautista <dj.bautista10@uniandes.edu.co>
2020-04-28 19:18:35 -0500

    asokdakld

---
+++ precache-manifest.c157043f7e5ee836ad99a0ab3fd024fd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe9d6502c312dc8b104c4a76c89322b",
+    "url": "/paper-dashboard-react/index.html"
+  },
+  {
+    "revision": "59e6cf30e063d8a41490",
+    "url": "/paper-dashboard-react/static/css/2.08c0db48.chunk.css"
+  },
+  {
+    "revision": "6f07166f9c077a203102",
+    "url": "/paper-dashboard-react/static/css/main.90132cfc.chunk.css"
+  },
+  {
+    "revision": "59e6cf30e063d8a41490",
+    "url": "/paper-dashboard-react/static/js/2.da736606.chunk.js"
+  },
+  {
+    "revision": "6f07166f9c077a203102",
+    "url": "/paper-dashboard-react/static/js/main.8d6888fa.chunk.js"
+  },
+  {
+    "revision": "b3fe53a14804cfc5877b",
+    "url": "/paper-dashboard-react/static/js/runtime~main.ff95ff9f.js"
+  },
+  {
+    "revision": "9880fc0845d0f3950e490e4f3b05c08c",
+    "url": "/paper-dashboard-react/static/media/JosefinSans-Bold.9880fc08.ttf"
+  },
+  {
+    "revision": "0d9b62a03206f739cd34b2936a5929f1",
+    "url": "/paper-dashboard-react/static/media/SourceSansPro-Bold.0d9b62a0.ttf"
+  },
+  {
+    "revision": "5182da425f811908bed9f5b8c72fa44f",
+    "url": "/paper-dashboard-react/static/media/SourceSansPro-Regular.5182da42.ttf"
+  },
+  {
+    "revision": "774c05ccae92d11b95a2ef722c9566a8",
+    "url": "/paper-dashboard-react/static/media/SourceSansPro-SemiBold.774c05cc.ttf"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/paper-dashboard-react/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/paper-dashboard-react/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/paper-dashboard-react/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/paper-dashboard-react/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/paper-dashboard-react/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/paper-dashboard-react/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
